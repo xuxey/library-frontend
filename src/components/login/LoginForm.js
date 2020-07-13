1613520e0523dd37f7fafa4da3c14650de9c2925
ThatXuxe <obuoy1804@gmail.com>
2020-07-14 01:30:33 +0530

    Use formik for LoginForm.js

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,50 +1,63 @@
 import React, {useState} from 'react'
 import {useMutation} from "@apollo/client";
 import {LOGIN_USER} from "../../mutations";
-import {Button, Col, Form} from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 import {useHistory} from "react-router-dom"
+import {ErrorMessage, Field, Form, Formik} from "formik";
 
 const LoginForm = ({setUser, setMessage}) => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
     const history = useHistory();
     const [loginUser] = useMutation(LOGIN_USER, {
             onError: error => setMessage(error.graphQLErrors[0].message, true)
         }
     )
-    const onSubmit = async (event) => {
-        event.preventDefault()
-        if (username === '') return
-        if (password === '') return
-        const user = {username, password}
-        let loggedInUser = await loginUser({variables: user})
-        if(!loggedInUser || !loggedInUser.data || !loggedInUser.data.login) {
-            setMessage('Username or password incorrect', true)
-            return
-        }
-        loggedInUser = loggedInUser.data.login
-        console.log("LOGGED IN USER", loggedInUser)
-        setUser(loggedInUser)
-        window.localStorage.setItem('libraryUser', JSON.stringify(loggedInUser))
-        setUsername('')
-        setPassword('')
-        setMessage(`Logged in as ${username}`, false)
-        history.push('/books')
-    }
+
     return (
-        <Form onSubmit={onSubmit}>
-            <Form.Row>
-                <Form.Group as={Col}>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control id='username' value={username} onChange={e => setUsername(e.target.value)}/>
-                </Form.Group>
-                <Form.Group as={Col}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control id='password' value={password} onChange={e => setPassword(e.target.value)} type='password'/>
-                </Form.Group>
-            </Form.Row>
-            <Button variant="primary" id="login-button" type="submit">Login</Button>
-        </Form>
+        <Formik
+            initialValues={{username: '', password:''}}
+            validate={values => {
+                const errors = {};
+                if (!values.username)
+                    errors.username = 'Required';
+                if (!values.password)
+                    errors.password = 'Required'
+                return errors;
+            }}
+            onSubmit={async (values, {setSubmitting}) => {
+                const user = {...values}
+                console.log("USER", user)
+                let loggedInUser = await loginUser({variables: user})
+                if (!loggedInUser || !loggedInUser.data || !loggedInUser.data.login) {
+                    setMessage('Username or password is incorrect', true)
+                    setSubmitting(true)
+                    return
+                }
+                loggedInUser = loggedInUser.data.login
+                setUser(loggedInUser)
+                window.localStorage.setItem('libraryUser', JSON.stringify(loggedInUser))
+                setMessage(`Logged in as ${values.username}`, false)
+                history.push('/books')
+                setSubmitting(false)
+            }}
+        >
+            {({ isSubmitting }) => (
+                <Form>
+                    <div className={"row"}>
+                        <div className={"form-group col"}>
+                            <div className="form-label">Username</div>
+                            <Field type="text" name="username" class="form-control"/>
+                            <ErrorMessage name="username" component="div" style={{color: "#ff0000"}} />
+                        </div>
+                        <div className={"form-group col"}>
+                            <div className="form-label">Password</div>
+                            <Field type="password" name="password" class="form-control"/>
+                            <ErrorMessage name="password" component="div" style={{color: "#ff0000"}}/>
+                        </div>
+                    </div>
+                    <Button type="submit" disabled={isSubmitting}>Submit</Button>
+                </Form>
+            )}
+        </Formik>
     )
 }
 
