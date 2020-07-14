e67b4bb6ebc14bc28ace375abf2b07fb482e2e33
ThatXuxe <obuoy1804@gmail.com>
2020-07-14 18:58:51 +0530

    Color code activity and make background sticky

--- src/components/books/Activity.js
+++ src/components/books/Activity.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useLazyQuery, useQuery} from "@apollo/client";
+import {useQuery} from "@apollo/client";
 import {GET_LOGS} from "../../queries";
 import {Table} from "react-bootstrap";
 
@@ -11,13 +11,21 @@ const getDateFromMillis = (time) => {
             date.getMonth()+'/'+
             date.getFullYear()
 }
-
+const parseType = (type) => {
+    switch (type) {
+        case "BOOK_RESERVE": return <div style={{color:"#03cefc"}}>Book Reserved</div>
+        case "USER_REGISTERED": return <div style={{color:"#a103fc"}}>User Registered</div>
+        case "SET_AVAILABLE": return <div style={{color:"#07fc03"}}>Set Available</div>
+        case "NEW_BOOK": return <div style={{color:"#fc8c03"}}>New Book Added</div>
+        default: return <div>Unknown</div>
+    }
+}
 const Activity = () => {
     const {data, loading, error} = useQuery(GET_LOGS,  {
         pollInterval: 5000,
     })
     if(loading || !data) return <div>Loading activity...</div>
-    if(error) return <div>An error has occured</div>
+    if(error) return <div>An error has occurred</div>
     let count = 1
     const logs = data.allLogs
     return (
@@ -33,7 +41,7 @@ const Activity = () => {
                 {
                     logs.map(log => <tr key={log._id}>
                         <td>{count++}</td>
-                        <td>{log.type}</td>
+                        <td>{parseType(log.type)}</td>
                         <td>{log.user.username}</td>
                         <td>{(log.book)? log.book.title: "N/A"}</td>
                         <td>{getDateFromMillis(log.time)}
