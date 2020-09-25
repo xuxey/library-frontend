f36e2846d5cd99e449e43b1b4e0d07df5a34b958
ThatXuxe <obuoy1804@gmail.com>
2020-09-25 22:31:33 +0530

    Disable account creation pre launch

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useMutation} from "@apollo/client";
 import {LOGIN_USER} from "../../mutations";
-import {Button} from "react-bootstrap";
+import {Alert, Button} from "react-bootstrap";
 import {useHistory} from "react-router-dom"
 import {ErrorMessage, Field, Form, Formik} from "formik";
 
@@ -41,11 +41,12 @@ const LoginForm = ({setUser, setMessage}) => {
         >
             {({ isSubmitting }) => (
                 <Form>
+                    <Alert variant={'danger'}> Creating accounts is temporarily disabled until launch</Alert>
                     <div className={"row"}>
                         <div className={"form-group col"}>
                             <div className="form-label">Username</div>
                             <Field type="text" name="username" class="form-control"/>
-                            <ErrorMessage name="username" component="div" style={{color: "#ff0000"}} />
+                            <ErrorMessage name="username" component="div" style={{color: "#ff0000"}}/>
                         </div>
                         <div className={"form-group col"}>
                             <div className="form-label">Password</div>
@@ -53,11 +54,12 @@ const LoginForm = ({setUser, setMessage}) => {
                             <ErrorMessage name="password" component="div" style={{color: "#ff0000"}}/>
                         </div>
                     </div>
-                    <Button type="submit" disabled={isSubmitting}>Submit</Button>
+                    <Button type="submit" disabled={true}>Submit</Button>
                 </Form>
             )}
         </Formik>
     )
+    //todo set that button prop to isSubmitting ^
 }
 
 export default LoginForm
